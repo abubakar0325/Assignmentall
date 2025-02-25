c38b70edf716599cd9e37c90130b4af5256d3881
Shyness <109921160+abubakar0325@users.noreply.github.com>
2025-02-25 21:21:40 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,25 +1,33 @@
+// Initialize 'datas' from localStorage or set default values  
 let datas = JSON.parse(localStorage.getItem('photos'));  
 
 if (!datas) {  
-  datas = [{  
-      image: '/images/FEB4CD45-2E42-4328-B224-D70A49CF1CCC-3762800813.jpeg',  
-      message: 'Barkanmu da wannan Lokachi'  
-  }, {  
-      image: '/images/ABBA-KABIR-YUSUF-CCB-1000x600-2088897328.jpeg',  
-      message: 'Barkanmu da wannan Lokachi'  
-  }, {  
-      image: '/images/Kwankwaso_HD-1528762212.jpeg',  
-      message: 'Barkanmu da wannan Lokachi'  
-  }, {  
-      image: '/images/Kwankwaso-4185211543.jpg',  
-      message: 'Barkanmu da wannan Lokachi'  
-  }, {  
-      image: '/images/ganduje-apc-1744930061.jpg',  
-      message: 'Barkanmu da wannan Lokachi'  
-  }, {  
-      image: "/images/ganduje...-3961510402.jpg",  
-      message: 'Barkanmu da wannan Lokachi'  
-  }];  
+    datas = [  
+        {  
+            image: '/images/FEB4CD45-2E42-4328-B224-D70A49CF1CCC-3762800813.jpeg',  
+            message: 'Barkanmu da wannan Lokachi'  
+        },  
+        {  
+            image: '/images/ABBA-KABIR-YUSUF-CCB-1000x600-2088897328.jpeg',  
+            message: 'Barkanmu da wannan Lokachi'  
+        },  
+        {  
+            image: '/images/Kwankwaso_HD-1528762212.jpeg',  
+            message: 'Barkanmu da wannan Lokachi'  
+        },  
+        {  
+            image: '/images/Kwankwaso-4185211543.jpg',  
+            message: 'Barkanmu da wannan Lokachi'  
+        },  
+        {  
+            image: '/images/ganduje-apc-1744930061.jpg',  
+            message: 'Barkanmu da wannan Lokachi'  
+        },  
+        {  
+            image: "/images/ganduje...-3961510402.jpg",  
+            message: 'Barkanmu da wannan Lokachi'  
+        }  
+    ];  
 }  
 
 // Function to save data to localStorage  
@@ -40,7 +48,7 @@ function showSlides() {
         slides[i].style.display = "none";  
     }  
     slideIndex++;  
-    if (slideIndex > slides.length) {slideIndex = 1}    
+    if (slideIndex > slides.length) { slideIndex = 1 }  
     slides[slideIndex - 1].style.display = "block";  
     setTimeout(showSlides, 4000); // Change image every 4 seconds  
 }  
@@ -52,7 +60,7 @@ datas.forEach((element) => {
           <img src="${element.image}" width="100%">  
           <p>${element.message}</p>  
       </div>`;  
-    
+
     projectHtml += `  
       <div>  
           <img src="${element.image}" width="100%">  
